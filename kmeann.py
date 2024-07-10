6667ba449c4305ec7196b5fbcb17ae9d70551477
Muhammad Yusuf <83113629+cornfuze@users.noreply.github.com>
2024-07-10 11:52:19 +0000

    Update kmeann.py

--- kmeann.py
+++ kmeann.py
@@ -5,7 +5,20 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-st.set_page_config(page_title="K-Means Clustering")
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.set_page_config(page_title="K-Means Clustering Kelompok 12")
 # Fungsi untuk memuat data
 @st.cache_data
 def muat_data():
